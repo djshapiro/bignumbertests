23d9206ce29161196e5a5070e6000bdf94560b0e
Dan Shapiro <daniel.j.shapiro@gmail.com>
2018-10-26 15:09:00 -0400

    use big big numbers

--- realUseCase.js
+++ realUseCase.js
@@ -8,33 +8,33 @@ const iterations = 20000;
 const myweb3 = new web3(web3.currentProvider);
 
 const web3Fn = (done) => {
-  let tok = new myweb3.BigNumber("10000000000");
-  let wei = new myweb3.BigNumber("300000000000");
-  let tot = new myweb3.BigNumber("4000000000000");
+  let tok = new myweb3.BigNumber("10000000000123456789123456789");
+  let wei = new myweb3.BigNumber("300000000000123456789123456789");
+  let tot = new myweb3.BigNumber("4000000000000123456789123456789");
   tot.mul(wei).div(tok).toString();
   done();
 }
 
 const BigNumberFn = (done) => {
-  let tok = new bignumber("10000000000");
-  let wei = new bignumber("300000000000");
-  let tot = new bignumber("4000000000000");
+  let tok = new bignumber("10000000000123456789123456789");
+  let wei = new bignumber("300000000000123456789123456789");
+  let tot = new bignumber("4000000000000123456789123456789");
   tot.mul(wei).div(tok).toString();
   done();
 }
 
 const ethersBigNumberFn = (done) => {
-  let tok = new ethers.utils.BigNumber("10000000000");
-  let wei = new ethers.utils.BigNumber("300000000000");
-  let tot = new ethers.utils.BigNumber("4000000000000");
+  let tok = new ethers.utils.BigNumber("10000000000123456789123456789");
+  let wei = new ethers.utils.BigNumber("300000000000123456789123456789");
+  let tot = new ethers.utils.BigNumber("4000000000000123456789123456789");
   tot.mul(wei).div(tok).toString();
   done();
 }
 
 const ethersBigNumberifyFn = (done) => {
-  let tok = new ethers.utils.bigNumberify("10000000000");
-  let wei = new ethers.utils.bigNumberify("300000000000");
-  let tot = new ethers.utils.bigNumberify("4000000000000");
+  let tok = new ethers.utils.bigNumberify("10000000000123456789123456789");
+  let wei = new ethers.utils.bigNumberify("300000000000123456789123456789");
+  let tot = new ethers.utils.bigNumberify("4000000000000123456789123456789");
   tot.mul(wei).div(tok).toString();
   done();
 }
